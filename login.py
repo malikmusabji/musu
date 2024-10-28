e333fc6fc9c087df508255a9f5ed9787c77fb47f
malikmusabji <117093080+malikmusabji@users.noreply.github.com>
2024-10-28 20:52:20 +0530

    Update login.py
    wrong

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sqlite3
 
-def login_page():
+ def login_page():
     st.markdown("<h1 style='text-align: center; color: #ff5733;'>PedoMUS</h1>", unsafe_allow_html=True)
     st.markdown("""
     ### Welcome to PedoMUS
